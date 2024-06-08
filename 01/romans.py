e2dd83798904ed4d702f9ea44c306b277aa0fef5
Vitor Gabriel <103908139+VitorGabrieldosSantosG@users.noreply.github.com>
2024-06-08 17:10:29 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -23,5 +23,20 @@ def int_to_roman(num):
 
 
 def roman_to_int(s):
-    # Implemente sua função aqui
-    pass
+        numeros = {
+        "M": 1000, "CM": 900, "D": 500, "CD": 400,
+        "C": 100, "XC": 90, "L": 50, "XL": 40,
+        "X": 10, "IX": 9, "V": 5, "IV": 4,
+        "I": 1
+    }
+    
+    i = 0
+    total = 0
+    while i < len(s):
+        if i + 1 < len(s) and s[i:i+2] in numeros:
+            total += numeros[s[i:i+2]]
+            i += 2
+        else:
+            total += numeros[s[i]]
+            i += 1
+    return total
