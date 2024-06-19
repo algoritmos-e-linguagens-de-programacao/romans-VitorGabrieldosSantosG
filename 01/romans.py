35ffd15d1f3851ba705a2dd4ff9a18acedc804b3
Vitor Gabriel <103908139+VitorGabrieldosSantosG@users.noreply.github.com>
2024-06-18 21:17:03 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -23,7 +23,7 @@ def int_to_roman(num):
 
 
 def roman_to_int(s):
-        numeros = {
+    numeros = {
         "M": 1000, "CM": 900, "D": 500, "CD": 400,
         "C": 100, "XC": 90, "L": 50, "XL": 40,
         "X": 10, "IX": 9, "V": 5, "IV": 4,
